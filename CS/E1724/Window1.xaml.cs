355be766e01883d4c9bf6034c637d5fff6469944
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/E1724/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using DevExpress.Xpf.Grid;
+
+namespace E1724 {
+    public partial class Window1 : Window {
+        CollectionViewSource source;
+        public Window1() {
+
+            InitializeComponent();
+
+            List<TestData> list = new List<TestData>();
+            for (int i = 0; i < 100; i++) {
+                list.Add(new TestData() {
+                    Number1 = i,
+                    Number2 = i * 10,
+                    Text1 = "row " + i,
+                    Text2 = "ROW " + i
+                });
+            }
+            source = new CollectionViewSource() { Source = list };
+            grid.ItemsSource = source.View;
+            listBox.ItemsSource = source.View;
+        }
+    }
+
+    public class TestData {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+    }
+}
---
+++ VB/E1724/Window1.xaml.vb
@@ -0,0 +1,67 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports DevExpress.Xpf.Grid
+
+Namespace E1724
+    Partial Public Class Window1
+        Inherits Window
+        Private source As CollectionViewSource
+        Public Sub New()
+
+            InitializeComponent()
+
+            Dim list As New List(Of TestData)()
+            For i As Integer = 0 To 99
+                list.Add(New TestData() With {.Number1 = i, .Number2 = i * 10, .Text1 = "row " & i, .Text2 = "ROW " & i})
+            Next i
+            Dim columnCount As Integer = grid.Columns.Count
+
+            source = New CollectionViewSource() With {.Source = list}
+            grid.ItemsSource = source.View
+            listBox.ItemsSource = source.View
+
+        End Sub
+    End Class
+
+    Public Class TestData
+        Private privateNumber1 As Integer
+        Public Property Number1() As Integer
+            Get
+                Return privateNumber1
+            End Get
+            Set(ByVal value As Integer)
+                privateNumber1 = value
+            End Set
+        End Property
+        Private privateNumber2 As Integer
+        Public Property Number2() As Integer
+            Get
+                Return privateNumber2
+            End Get
+            Set(ByVal value As Integer)
+                privateNumber2 = value
+            End Set
+        End Property
+        Private privateText1 As String
+        Public Property Text1() As String
+            Get
+                Return privateText1
+            End Get
+            Set(ByVal value As String)
+                privateText1 = value
+            End Set
+        End Property
+        Private privateText2 As String
+        Public Property Text2() As String
+            Get
+                Return privateText2
+            End Get
+            Set(ByVal value As String)
+                privateText2 = value
+            End Set
+        End Property
+    End Class
+End Namespace
