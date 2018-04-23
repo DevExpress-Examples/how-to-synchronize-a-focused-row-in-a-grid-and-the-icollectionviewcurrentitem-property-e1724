f06f324e402eaf0c568a9d5ae7520ad1bda023be
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to Synchronize a Focused Row in a Grid and the ICollectionView.CurrentItem property
+
+
+<p>This example shows how to synchronize a focused row in a grid control with a selected item in a standard list box (the grid and list box are bound to the same data source).<br />
+Starting from version 2011 vol.2, you need to enable the TableView.IsSynchronizedWithCurrentItem property to synchronize ICollectionView CurrentItem with grid CurrentItem.</p>
+
+<br/>
+
+
---
+++ VB/DXGrid_FocusedRow/Window1.xaml.vb
@@ -0,0 +1,75 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports DevExpress.Xpf.Grid
+
+Namespace DXGrid_FocusedRow
+	Partial Public Class Window1
+		Inherits Window
+		Private source As CollectionViewSource
+		Public Sub New()
+
+			InitializeComponent()
+
+			Dim list As New List(Of TestData)()
+			For i As Integer = 0 To 99
+				list.Add(New TestData() With {.Number1 = i, .Number2 = i * 10, .Text1 = "row " & i, .Text2 = "ROW " & i})
+			Next i
+			Dim columnCount As Integer = grid.Columns.Count
+
+			source = New CollectionViewSource() With {.Source = list}
+			AddHandler source.View.CurrentChanged, AddressOf CollectionView_CurrentChanged
+			AddHandler view.FocusedRowChanged, AddressOf GridView_FocusedRowChanged
+			grid.ItemsSource = list
+			listBox.ItemsSource = source.View
+
+		End Sub
+		Private Sub GridView_FocusedRowChanged(ByVal sender As Object, ByVal e As FocusedRowChangedEventArgs)
+			source.View.MoveCurrentTo(grid.GetRow(view.FocusedRowHandle))
+		End Sub
+		Private Sub CollectionView_CurrentChanged(ByVal sender As Object, ByVal e As System.EventArgs)
+			view.FocusedRowHandle = grid.GetRowHandleByListIndex(source.View.CurrentPosition)
+		End Sub
+	End Class
+
+	Public Class TestData
+		Private privateNumber1 As Integer
+		Public Property Number1() As Integer
+			Get
+				Return privateNumber1
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber1 = value
+			End Set
+		End Property
+		Private privateNumber2 As Integer
+		Public Property Number2() As Integer
+			Get
+				Return privateNumber2
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber2 = value
+			End Set
+		End Property
+		Private privateText1 As String
+		Public Property Text1() As String
+			Get
+				Return privateText1
+			End Get
+			Set(ByVal value As String)
+				privateText1 = value
+			End Set
+		End Property
+		Private privateText2 As String
+		Public Property Text2() As String
+			Get
+				Return privateText2
+			End Get
+			Set(ByVal value As String)
+				privateText2 = value
+			End Set
+		End Property
+	End Class
+End Namespace
